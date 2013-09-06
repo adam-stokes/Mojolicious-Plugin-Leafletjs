088548336b3bf99adf8323b8811f2dc0d8aabcd8
Adam Stokes <adam.stokes@ubuntu.com>
2013-09-05 21:56:46 -0400

    0.004

--- lib/Mojolicious/Plugin/Leafletjs.pm
+++ lib/Mojolicious/Plugin/Leafletjs.pm
@@ -5,7 +5,7 @@ use File::Basename 'dirname';
 use File::Spec::Functions 'catdir';
 use File::ShareDir ':ALL';
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 my %defaults = (
     name      => 'map',
